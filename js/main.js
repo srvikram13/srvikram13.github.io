0687a9a5bae9fc34145342ecf9c61c18d7c15267
Vikram Deshmukh <srvikram13@gmail.com>
2018-02-19 17:56:28 -0800

    Updated portfolio projects styling to a more card-like UI.

--- js/main.js
+++ js/main.js
@@ -40,3 +40,51 @@ animMeter();
             }
         });
     });
+$(document).ready(function(){
+  var zindex = 10;
+  
+  $("div.card").click(function(e){
+    console.log(e.target, e.currentTarget)
+    if($(e.target).hasClass("btn")) {
+        return;
+    }
+    e.preventDefault();
+
+    var isShowing = false;
+
+    if ($(this).hasClass("show")) {
+      isShowing = true
+    }
+
+    if ($("div.cards").hasClass("showing")) {
+      // a card is already in view
+      $("div.card.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        // this card was showing - reset the grid
+        $("div.cards")
+          .removeClass("showing");
+      } else {
+        // this card isn't showing - get in with it
+        $(this)
+          .css({zIndex: zindex})
+          .addClass("show");
+
+      }
+
+      zindex++;
+
+    } else {
+      // no cards in view
+      $("div.cards")
+        .addClass("showing");
+      $(this)
+        .css({zIndex:zindex})
+        .addClass("show");
+
+      zindex++;
+    }
+    
+  });
+});
